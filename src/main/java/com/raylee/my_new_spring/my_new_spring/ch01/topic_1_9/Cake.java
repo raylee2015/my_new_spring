ae954ad7212494c05345b38456511cd3c0bb7bcc
raylee2007 <lilinghui-123@163.com>
2016-01-31 22:59:07 +0800

    从头认识Spring-1.9 内部类注入Bean

---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch01/topic_1_9/Cake.java
@@ -0,0 +1,37 @@
+package com.raylee.my_new_spring.my_new_spring.ch01.topic_1_9;
+
+public class Cake {
+
+	private final int id = index++;
+
+	private static int index = 0;
+
+	private String name = "";
+
+	private double size = 0;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "create the cake,its id:" + id + ", size:" + size + " inch ,name:" + name;
+	}
+}
---
+++ src/test/java/com/raylee/my_new_spring/my_new_spring/ch01/topic_1_9/ChiefTest.java
@@ -0,0 +1,24 @@
+package com.raylee.my_new_spring.my_new_spring.ch01.topic_1_9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"/com/raylee/my_new_spring/my_new_spring/ch01/topic_1_9/ApplicationContext-test.xml" })
+public class ChiefTest {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void testChief() {
+		Chief chief = applicationContext.getBean(Chief.class);
+		System.out.println("chief name:" + chief.getName());
+		System.out.println(chief.getName() + " make a cake ,cake's name :" + chief.makeOneCake());
+	}
+}
