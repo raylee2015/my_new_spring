dfb6fd13561846825f96c1a28b3e7868bd4cd434
raylee2007 <lilinghui-123@163.com>
2016-02-11 23:39:59 +0800

    从头认识Spring-2.1 自动装配（2）-byType

---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_2/Chief.java
@@ -0,0 +1,29 @@
+package com.raylee.my_new_spring.my_new_spring.ch02.topic_1_2;
+
+public class Chief {
+	private Cake cake = null;
+
+	private String name = "";
+
+	public Cake getCake() {
+		return cake;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Cake makeOneCake() {
+		System.out.println(getName() + " make " + getCake().getName());
+		return cake;
+	}
+
+	public void setCake(Cake cake) {
+		this.cake = cake;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
