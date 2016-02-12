a7a949613a77bfdf22c417cd21e42b6a28625e65
raylee2007 <lilinghui-123@163.com>
2016-02-12 09:21:49 +0800

    从头认识Spring-2.2 默认自动装配 第一种配置-标准配置

---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_5/Chief.java
@@ -0,0 +1,25 @@
+package com.raylee.my_new_spring.my_new_spring.ch02.topic_1_5;
+
+public class Chief {
+	private Cake cake = null;
+
+	private String name = "";
+
+	public Chief(Cake cake) {
+		this.cake = cake;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Cake makeOneCake() {
+		System.out.println(getName() + " make " + cake.getName());
+		return cake;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/test/java/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_5/ChiefTest.java
@@ -0,0 +1,23 @@
+package com.raylee.my_new_spring.my_new_spring.ch02.topic_1_5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_5/ApplicationContext-test.xml" })
+public class ChiefTest {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void testChief() {
+		Chief jack = (Chief) applicationContext.getBean("jack");
+		jack.makeOneCake();
+	}
+}
