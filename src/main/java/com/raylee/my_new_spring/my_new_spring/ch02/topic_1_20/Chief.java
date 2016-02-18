8609aa71cfff59898f265ff5c410b835288a0d16
raylee2015 <lilinghui-123@163.com>
2016-02-18 14:39:18 +0800

    从头认识Spring-2.7 自动检测Bean（2）-过滤器<context:include-filter/>

---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_20/Chief.java
@@ -0,0 +1,18 @@
+package com.raylee.my_new_spring.my_new_spring.ch02.topic_1_20;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Chief implements Person {
+
+	@Value("jack")
+	private String name = "";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch02/topic_1_20/Person.java
@@ -0,0 +1,4 @@
+package com.raylee.my_new_spring.my_new_spring.ch02.topic_1_20;
+
+public interface Person {
+}
