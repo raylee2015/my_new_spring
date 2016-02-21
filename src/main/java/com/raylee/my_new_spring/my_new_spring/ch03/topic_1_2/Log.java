6583f6cdecdccf6ae4a3e0a11c1b0d1574bf9666
raylee2007 <lilinghui-123@163.com>
2016-02-21 23:05:23 +0800

    从头认识Spring-3.1 简单的AOP日志实现（2）需要记录方法的运行时间

---
+++ src/main/java/com/raylee/my_new_spring/my_new_spring/ch03/topic_1_2/Log.java
@@ -0,0 +1,29 @@
+package com.raylee.my_new_spring.my_new_spring.ch03.topic_1_2;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class Log {
+
+	public void washOven() {
+		System.out.println("washOven,logging.....");
+	}
+
+	public void prepare() {
+		System.out.println("prepare,logging.....");
+	}
+
+	public void after() {
+		System.out.println("after someting to do,logging.....");
+	}
+
+	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+		washOven();
+		prepare();
+		long startTime = System.currentTimeMillis();
+		joinPoint.proceed();
+		long endTime = System.currentTimeMillis();
+		System.out.println("use time:" + (endTime - startTime));
+		after();
+	}
+
+}
